2b7c932e1af522530ab18be78859d0e95e1f45f4
erica cha & chitra <student@epicodus.com>
2015-04-22 12:23:25 -0700

    add findId on utilities factory. page functions accordingly.

--- services/CoursesFactory.js
+++ services/CoursesFactory.js
@@ -1,10 +1,17 @@
 courseRoster.factory('CoursesFactory', function CoursesFactory() {
-  var factory = {};
-  factory.courses = [];
-  factory.addCourse = function() {
-    factory.courses.push({ name: factory.courseName, id: factory.courses.length + 1, students: [] });
-    factory.courseName = null;
-  };
+    var factory = {};
+    factory.courses = [];
 
-  return factory;
+    //push input info into courses
+    factory.addCourse = function() {
+        var course = { name: factory.courseName, id:factory.courses.length + 1, students:[] };
+        //factory.courses.push({ name: factory.courseName, id: factory.courses.length + 1, students: [] });
+        factory.courses.push(course);
+        factory.courseName = null;
+    };
+
+
+
+    return factory;
+
 });
